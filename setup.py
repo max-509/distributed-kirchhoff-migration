1e131bc92f390dbc4c09a7ae5ef2970e73a181bd
GLEB2001 <6777gleb7776@gmail.com>
2022-02-05 21:42:12 +0700

    Не работает

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-from glob import glob
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension
 
 with open('requirements.txt') as f:
     packages_required = f.read().splitlines()
@@ -10,17 +9,16 @@ ext_modules = [
     Pybind11Extension(
         "_migration",
         ['Migration_on_C/Migration.cpp']
-        # sorted(glob('cpp_extensions/Migration_on_C/*.cpp'))
     )
 ]
 
 
 setup(
     name='distributed-kirchhoff-migration',
-    # version='0.0.1',
-    # description='MPI application of kirchhoff migration implementation',
-    # author='Vershinin Maxim',
-    # author_email='123456vershinin@gmail.com',
+    version='0.0.1',
+    description='MPI application of kirchhoff migration implementation',
+    author='Vershinin Maxim, Zobnin Gleb, Dzharkinov Ruslan, Konyukhov Grigoriy',
+    author_email='123456vershinin@gmail.com, 6777gleb7776@gmail.com, dzharkinovr@gmail.ru, konyukhovgm@gmail.com',
     scripts=['mpi_migration.py'],
     install_requires=packages_required,
     ext_modules=ext_modules
